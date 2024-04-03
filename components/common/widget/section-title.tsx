ebd3a022945e513243453c9e232158b9320302d5
IfratBinte <ifratbinte@gmail.com>
2024-04-03 23:19:18 +0600

    feat : create section header bottom border

---
+++ components/common/widget/section-title.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./widget.module.css"
+
+interface TitleProps{
+    title: string
+}
+
+const SectionTitle:React.FC<TitleProps> = ({title}) => {
+  return (
+    <div className="relative">
+        <div className={`${styles.section_border} text-4xl font-semibold text-center pb-5 mb-10`}>{title}</div>
+    </div>
+  )
+}
+
+export default SectionTitle
